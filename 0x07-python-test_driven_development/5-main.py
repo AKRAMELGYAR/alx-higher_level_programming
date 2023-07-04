6a5280fd54727c8bd36bbe20126f1406ec33fd56
akram elgyar <akramelgyar@gmail.com>
2023-07-04 16:59:52 -0700

    update

---
+++ 0x07-python-test_driven_development/5-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+ansfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+raid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
